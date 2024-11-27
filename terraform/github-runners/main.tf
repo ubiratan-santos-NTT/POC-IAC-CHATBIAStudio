e50047bcee988034a811235f256da598bd3830a0
Bira <ugleidso@emeal.nttdata.com>
2024-11-27 03:37:58 +0000

    add var backend

--- terraform/github-runners/main.tf
+++ terraform/github-runners/main.tf
@@ -14,6 +14,12 @@ terraform {
 
 provider "azurerm" {
   features {}
+  backend "azurerm" {
+    resource_group_name   = var.resource_group_name
+    storage_account_name  = var.storage_account_name
+    container_name        = var.container_name
+    key                   = var.key
+  }
 }
 
 
