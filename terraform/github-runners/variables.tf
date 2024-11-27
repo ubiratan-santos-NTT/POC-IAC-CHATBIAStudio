d5aa65a1ba9c3c25473a619bc4de845bcc100ca0
Bira <ugleidso@emeal.nttdata.com>
2024-11-27 04:07:13 +0000

    add variables

--- terraform/github-runners/variables.tf
+++ terraform/github-runners/variables.tf
@@ -2,3 +2,18 @@ variable "github_token" {
     type = string
 }
 
+variable "resource_group_name" {
+    type = string
+}
+
+variable "storage_account_name" {
+    type = string
+}
+
+variable "container_name" {
+    type = string
+}
+
+variable "key" {
+    type = string
+}
